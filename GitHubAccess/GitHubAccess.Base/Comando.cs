5e7e6cec553d3cd3daabaa1b4057a16a218219fc
Mateus Kern <mateus.g.kern@gmail.com>
2023-02-15 19:43:44 -0300

    Começando a estrutura para separar as responsabilidades

---
+++ GitHubAccess/GitHubAccess.Base/Comando.cs
@@ -0,0 +1,9 @@
+﻿using Flunt.Notifications;
+
+namespace GitHubAccess.Base
+{
+    public abstract class Comando : Notifiable<Notification>
+    {
+        public abstract void Validate();
+    }
+}
---
+++ GitHubAccess/GitHubAccess.Base/IManipulador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubAccess.Base
+{
+    public interface IManipulador<T> where T : Comando
+    {
+        Task</*IResultado*/object> ManipularAsync(T comando);
+    }
+}
