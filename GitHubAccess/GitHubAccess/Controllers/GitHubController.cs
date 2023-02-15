a0730543ebca93050061142008a2a623106af6e6
Mateus Kern <mateus.g.kern@gmail.com>
2023-02-15 19:00:28 -0300

    Buscando repositoriois no github

---
+++ GitHubAccess/GitHubAccess/Controllers/GitHubController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace GitHubAccess.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+
+        [HttpGet(Name = "busca-lista-repos")]
+        public async Task<IActionResult> Get()
+        {
+            List<Repository> repositorios = new List<Repository>();
+
+            repositorios.Add(await BuscaRepositorioGitHub(Language.CSharp));
+            repositorios.Add(await BuscaRepositorioGitHub(Language.Python));
+            repositorios.Add(await BuscaRepositorioGitHub(Language.JavaScript));
+            repositorios.Add(await BuscaRepositorioGitHub(Language.Java));
+            repositorios.Add(await BuscaRepositorioGitHub(Language.Css));
+
+            return Ok(repositorios);
+        }
+
+        private async Task<Repository> BuscaRepositorioGitHub(Language language)
+        {
+            var githubClient = new GitHubClient(new ProductHeaderValue("GitHubAccess"));
+
+            var request = new SearchRepositoriesRequest
+            {
+                Language = language,
+                SortField = RepoSearchSort.Stars,
+                Order = SortDirection.Descending,
+                PerPage = 1,
+                Page = 1
+            };
+
+            return (await githubClient.Search.SearchRepo(request)).Items[0];
+        }
+    }
+}
